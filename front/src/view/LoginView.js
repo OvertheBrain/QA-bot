72ee3a7cc7a8c6a0ae90afff8f840ecfc7d0121b
zxy <923358767@qq.com>
2021-07-13 17:19:04 +0800

    test

---
+++ front/src/view/LoginView.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Button} from 'react-native-elements';
+
+
+class LoginView extends React.Component{
+
+    render() {
+        return (
+            <Button
+
+                title="Login"
+            />
+        )
+    }
+}
+export default LoginView
