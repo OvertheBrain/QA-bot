6d44eae96b97877b0f5769cf8b1c7a143ab2fe5b
zxy <923358767@qq.com>
2021-07-14 10:45:49 +0800

    front

--- front/src/view/LoginView.js
+++ front/src/view/LoginView.js
@@ -1,30 +1,52 @@
 import React from 'react';
-import {Button,SocialIcon,ThemeProvider,Divider} from 'react-native-elements';
-import {Text} from 'react-native';
+import {Button,SocialIcon,ThemeProvider,Divider,Tile,Text} from 'react-native-elements';
+import {StyleSheet} from 'react-native';
+
 import Icon from 'react-native-vector-icons/FontAwesome';
 
+const
+    styles=StyleSheet.create({
+        container: {
+            justifyContent:'flex-end',
+            height:'100%'
+        },
+        button:{
+            width:'50%'
+        }
 
+    })
 class LoginView extends React.Component{
+
+
 
     render() {
         return (
-            <ThemeProvider>
+
+            <Divider style={styles.container}>
+                <Text h1 h1Style={{textAlign:"center",marginBottom:200,color:"#1d3f63"}}>
+                    QA Robot
+                </Text>
+
 
                 <Button
+                    buttonStyle={{height:60,borderRadius:20}}
+                    containerStyle={{ marginHorizontal:30 ,marginVertical:10}}
                     title="Login"
+                    type="outline"
+
+                />
+
+                <Button
+                    buttonStyle={{height:60,borderRadius:20}}
+                    containerStyle={{ marginHorizontal:30 ,marginVertical:10 }}
+                    title="Register"
                     type="outline"
                 />
 
-                <Button
-                    title="Register"
-                    type="clear"
-                />
 
 
 
-
-
-            </ThemeProvider>
+            </Divider>
         )
     }
 }
