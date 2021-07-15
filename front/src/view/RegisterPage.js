493ce8e189e37ac64f6bfbd34c50f2fe497affdc
lq-loser <2512618003@qq.com>
2021-07-15 13:43:00 +0800

    Login and Register first version

--- front/src/view/RegisterPage.js
+++ front/src/view/RegisterPage.js
@@ -1,6 +1,41 @@
-import React from 'react'
+import React from 'react';
+import {ScrollView, View} from 'react-native';
+import {Avatar, Button} from 'react-native-elements';
+import {Input} from 'react-native-elements/dist/input/Input';
 
-class RegisterPage extends React.Component{
-
+class RegisterPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      navigation: this.props.navigation,
+    };
+  }
+  render() {
+    return (
+      <ScrollView>
+        <View style={{flex: 2}}>
+          <Avatar size={'xlarge'} rounded title="Register" />
+        </View>
+        <View style={{flex: 1}}>
+          <Input placeholder={'User'} />
+        </View>
+        <View style={{flex: 1}}>
+          <Input placeholder={'Password'} secureTextEntry={true} />
+        </View>
+        <View style={{flex: 1}}>
+          <Input placeholder={'Password again'} secureTextEntry={true} />
+        </View>
+        <View style={{flex: 1}}>
+          <Button
+            buttonStyle={{height: 60, borderRadius: 20}}
+            containerStyle={{marginHorizontal: 30, marginVertical: 10}}
+            title="Register"
+            type="outline"
+            onPress={() => this.state.navigation.navigate('Home')}
+          />
+        </View>
+      </ScrollView>
+    );
+  }
 }
 export default RegisterPage;
