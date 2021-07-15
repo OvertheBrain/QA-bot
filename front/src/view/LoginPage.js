a6699df4299ae308100c04c401863af8cbf34424
lq-loser <2512618003@qq.com>
2021-07-15 15:11:17 +0800

    add the icon and header(can return to the startPage)

--- front/src/view/LoginPage.js
+++ front/src/view/LoginPage.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import {ScrollView, View} from 'react-native';
-import {Avatar, Button} from 'react-native-elements';
+import {Avatar, Button, Header} from 'react-native-elements';
+import {Text} from 'react-native-elements';
+import {Icon} from 'react-native-elements/dist/icons/Icon';
 import {Input} from 'react-native-elements/dist/input/Input';
 
 class LoginPage extends React.Component {
@@ -10,21 +12,54 @@ class LoginPage extends React.Component {
       navigation: this.props.navigation,
     };
   }
+
   render() {
     return (
       <ScrollView>
+        <View style={{flex: 1}}>
+          <Header
+            leftComponent={
+              <Icon
+                name="left"
+                type="antdesign"
+                onPress={() => {
+                  this.state.navigation.navigate('Start');
+                }}
+              />
+            }
+          />
+        </View>
         <View style={{flex: 2}}>
-          <Avatar size={'xlarge'} rounded title="Bot" />
+          <Text
+            h1
+            h1Style={{
+              textAlign: 'center',
+              marginTop: 50,
+              marginBottom: 20,
+              color: '#1d3f63',
+              fontSize: 43,
+            }}>
+            Login
+          </Text>
         </View>
         <View style={{flex: 1}}>
-          <Input placeholder={'User'} />
+          <Input
+            placeholder={'User'}
+            leftIcon={<Icon name="user" type="font-awesome" color="#1d3f63" />}
+          />
         </View>
         <View style={{flex: 1}}>
-          <Input placeholder={'Password'} secureTextEntry={true} />
+          <Input
+            placeholder={'Password'}
+            secureTextEntry={true}
+            leftIcon={
+              <Icon name={'lock'} type="font-awesome" color="#1d3f63" />
+            }
+          />
         </View>
         <View style={{flex: 1}}>
           <Button
-            buttonStyle={{height: 60, borderRadius: 20}}
+            buttonStyle={{height: 60, borderRadius: 20, marginTop: 10}}
             containerStyle={{marginHorizontal: 30, marginVertical: 10}}
             title="Login"
             type="outline"
--- front/src/view/RegisterPage.js
+++ front/src/view/RegisterPage.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import {ScrollView, View} from 'react-native';
-import {Avatar, Button} from 'react-native-elements';
+import {Avatar, Button, Header} from 'react-native-elements';
+import {Text} from 'react-native-elements';
 import {Input} from 'react-native-elements/dist/input/Input';
+import {Icon} from 'react-native-elements/dist/icons/Icon';
 
 class RegisterPage extends React.Component {
   constructor(props) {
@@ -13,22 +15,63 @@ class RegisterPage extends React.Component {
   render() {
     return (
       <ScrollView>
-        <View style={{flex: 2}}>
-          <Avatar size={'xlarge'} rounded title="Register" />
+        <View style={{flex: 1}}>
+          <Header
+            leftComponent={
+              <Icon
+                name="left"
+                type="antdesign"
+                onPress={() => {
+                  this.state.navigation.navigate('Start');
+                }}
+              />
+            }
+          />
         </View>
         <View style={{flex: 1}}>
-          <Input placeholder={'User'} />
+          <Text
+            h1
+            h1Style={{
+              textAlign: 'center',
+              marginTop: 50,
+              marginBottom: 20,
+              color: '#1d3f63',
+            }}>
+            Register
+          </Text>
         </View>
         <View style={{flex: 1}}>
-          <Input placeholder={'Password'} secureTextEntry={true} />
+          <Input
+            placeholder={'Username'}
+            leftIcon={<Icon name="user" type="font-awesome" color="#1d3f63" />}
+          />
         </View>
         <View style={{flex: 1}}>
-          <Input placeholder={'Password again'} secureTextEntry={true} />
+          <Input
+            placeholder={'Password'}
+            secureTextEntry={true}
+            leftIcon={
+              <Icon name={'lock'} type="font-awesome" color="#1d3f63" />
+            }
+          />
+        </View>
+        <View style={{flex: 1}}>
+          <Input
+            placeholder={'Password again'}
+            secureTextEntry={true}
+            leftIcon={
+              <Icon name={'lock'} type="font-awesome" color="#1d3f63" />
+            }
+          />
         </View>
         <View style={{flex: 1}}>
           <Button
             buttonStyle={{height: 60, borderRadius: 20}}
-            containerStyle={{marginHorizontal: 30, marginVertical: 10}}
+            containerStyle={{
+              marginHorizontal: 30,
+              marginVertical: 10,
+              marginTop: 15,
+            }}
             title="Register"
             type="outline"
             onPress={() => this.state.navigation.navigate('Home')}
