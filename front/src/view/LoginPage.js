2a2a50dcbdd6658c82169a06942e473b4e679cc5
lq-loser <2512618003@qq.com>
2021-07-15 15:59:56 +0800

    password can change visibility and add the remember option

--- front/src/view/LoginPage.js
+++ front/src/view/LoginPage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {ScrollView, View} from 'react-native';
-import {Avatar, Button, Header} from 'react-native-elements';
+import {Avatar, Button, CheckBox, Header} from 'react-native-elements';
 import {Text} from 'react-native-elements';
 import {Icon} from 'react-native-elements/dist/icons/Icon';
 import {Input} from 'react-native-elements/dist/input/Input';
@@ -10,10 +10,14 @@ class LoginPage extends React.Component {
     super(props);
     this.state = {
       navigation: this.props.navigation,
+      checked: false,
+      visible: true,
+      visIcon: true,
     };
   }
 
   render() {
+    const eyeIcon = ['lock', 'eye'];
     return (
       <ScrollView>
         <View style={{flex: 1}}>
@@ -51,10 +55,32 @@ class LoginPage extends React.Component {
         <View style={{flex: 1}}>
           <Input
             placeholder={'Password'}
-            secureTextEntry={true}
+            secureTextEntry={this.state.visible}
             leftIcon={
               <Icon name={'lock'} type="font-awesome" color="#1d3f63" />
             }
+            rightIcon={
+              <Icon
+                name={this.state.visIcon ? eyeIcon[0] : eyeIcon[1]}
+                type={'font-awesome'}
+                color="#1d3f63"
+                onPress={() => {
+                  this.setState({
+                    visible: !this.state.visible,
+                    visIcon: !this.state.visIcon,
+                  });
+                }}
+              />
+            }
+          />
+        </View>
+        <View>
+          <CheckBox
+            title={'Remember'}
+            checked={this.state.checked}
+            onPress={() => {
+              this.setState({checked: !this.state.checked});
+            }}
           />
         </View>
         <View style={{flex: 1}}>
