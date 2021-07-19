16ec71a80593504be6c186f7b13566ee261798ea
hanyiudo <hanyiduo31@126.com>
2021-07-19 09:39:53 +0800

    chat

---
+++ front/src/component/ChatCom/ChatRoomScreen.js
@@ -0,0 +1,93 @@
+import React, { useState, useCallback, useEffect } from 'react';
+import { GiftedChat,Bubble,Send } from 'react-native-gifted-chat';
+import 'dayjs/locale/zh-cn';
+import {View,Text,StyleSheet,SafeAreaView} from 'react-native';
+
+export default function ChatRoomScreen() {
+    const [messages, setMessages] = useState([]);
+    useEffect(() => {
+        setMessages([
+            {
+                _id: 1,
+                text: '开始聊天吧！',
+                createdAt: new Date(),
+                user: {
+                    _id: 2,
+                    name: 'mm',
+                    avatar: 'https://placeimg.com/140/140/any',
+                },
+            },
+        ])
+    }, []);
+    const onSend = useCallback((msg = []) => {
+        setMessages(previousMessages => GiftedChat.append(previousMessages, msg))
+    }, []);
+
+    const renderBubble = (props) => {
+        return (
+            <Bubble
+                {...props}
+                textStyle={{
+                    right: {
+                        color: 'black',
+                    },
+                }}
+                wrapperStyle={{
+                    left: {
+                        backgroundColor: '#fff',
+                    },
+                    right: {
+                        backgroundColor: '#0779f3',
+                    },
+                }}
+            />
+        );
+    };
+
+    const renderSend = (props) => {
+        return (
+            <Send
+                {...props}
+                alwaysShowSend={true}
+            >
+                <View style={styles.sendBtn}>
+                    <Text style={{color: '#ffffff', fontSize: 17}}>Send</Text>
+                </View>
+            </Send>
+        );
+    };
+
+    return (
+        <GiftedChat
+            messages={messages}
+            onSend={messages => onSend(messages)}
+            showUserAvatar={true}
+            locale={'zh-cn'}
+            showAvatarForEveryMessage={true}
+            renderBubble={renderBubble}
+            placeholder={'开始聊天吧'}
+            renderSend={renderSend}
+            inverted={true}
+            renderUsernameOnMessage={true}
+            user={{
+                _id: 50,
+                name: '阳光',
+                avatar: 'https://placeimg.com/140/140/any',
+            }}
+            alignTop={true}
+        />
+    )
+}
+const styles = StyleSheet.create({
+    sendBtn: {
+        width: 63,
+        height: 32,
+        borderRadius: 3,
+        backgroundColor:'#0779f3',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginBottom:5,
+        marginRight:5,
+    }
+});
+
---
+++ front/src/view/ChatPage.js
@@ -0,0 +1,82 @@
+import React from 'react';
+import {Header, SpeedDial} from 'react-native-elements';
+import {StyleSheet, ScrollView, View} from 'react-native';
+import ChatRoomScreen from '../component/ChatRoomScreen';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+const styles = StyleSheet.create({
+    container: {
+        flex:1,
+    },
+    options: {
+        marginBottom:50
+    },
+});
+
+export class ChatPage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            navigation: this.props.navigation,
+            robotName: 'Miku',
+            OptionsOpen: 0
+        };
+    }
+
+    OpenDial=(isopen)=>{
+        this.setState({OptionsOpen: isopen});
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Header
+                    leftComponent={<Icon
+                        name="arrow-circle-o-left"
+                        type="Ionicons"
+                        //onPress={() => {
+                        //this.state.navigation.navigate('Start');
+                        //}}
+                        color={'#fff'}
+                        size={40}
+                    />}
+                    centerComponent={{ text: this.state.robotName, style: { color: '#fff',fontSize: 30 }  }}
+                    rightComponent={<Icon
+                        name="info"
+                        type="AntDesign"
+                        //onPress={() => {
+                        //this.state.navigation.navigate('Start');
+                        //}}
+                        color={'#fff'}
+                        size={40}
+                    />}
+                />
+
+                <ChatRoomScreen/>
+                <SpeedDial style={styles.options}
+                           isOpen={this.state.OptionsOpen}
+                           icon={{ name: 'add', color: '#fff' }}
+                           openIcon={{ name: 'close', color: '#fff' }}
+                           onOpen={() => this.OpenDial(1)}
+                           onClose={() => this.OpenDial(0)}
+                           color={'#1E56EA'}
+                >
+                    <SpeedDial.Action
+                        icon={{ name: 'add', color: '#fff' }}
+                        title="Add"
+                        onPress={() => console.log('Add Something')}
+                        color={'#1E56EA'}
+                    />
+                    <SpeedDial.Action
+                        icon={{ name: 'delete', color: '#fff' }}
+                        title="Delete"
+                        onPress={() => console.log('Delete Something')}
+                        color={'#1E56EA'}
+                    />
+                </SpeedDial>
+            </View>
+        );
+    }
+}
+
+export default ChatPage;
