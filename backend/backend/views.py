5ca2badcfab12aa594b5661f07bd2acb3c77ccdf
hanyiudo <hanyiduo31@126.com>
2021-07-23 17:02:56 +0800

    delete backend

--- backend/backend/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from .qa import qa
-
-
-def hello(request):
-    context = {'reply': qa(request.GET.get('message'))}
-    return render(request, 'index.html', context)
