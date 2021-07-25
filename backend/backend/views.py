614600bb3d145e5efbc4d411f7d868323cc926cb
lq-loser <2512618003@qq.com>
2021-07-26 01:36:59 +0800

    可以使用chatterbot聊天

---
+++ backend/backend/views.py
@@ -0,0 +1,47 @@
+import json
+from django.views.generic.base import TemplateView
+from django.views.generic import View
+from chatterbot.ext.django_chatterbot import settings
+from django.http import HttpResponse, JsonResponse
+from django.shortcuts import render
+from chatterbot import ChatBot
+
+from .qa import qa
+
+
+class ChatBotApiView(View):
+    chatterbot = ChatBot(**settings.CHATTERBOT)
+    chatterbot.storage.drop()
+    # def api(self, request, *args, **kwargs):
+    #     # x = request.POST.get('message')
+    #     # return qa(x)
+    #     input_data = request.POST.get('message')
+    #     response = self.chatterbot.get_response(input_data)
+    #
+    #     response_data = response.serialize()
+    #     return JsonResponse(response_data, status=200)
+    def post(self, request, *args, **kwargs):
+        # x = request.POST.get('message')
+        # return qa(x)
+        input_data = json.loads(request.body.decode('utf-8'))
+        response = self.chatterbot.get_response(input_data)
+
+        # response_data = response.serialize()
+        res = {
+            'reply': response.text
+        }
+        return HttpResponse(json.dumps(res), content_type='application/json')
+
+    def get(self, request, *args, **kwargs):
+        """
+        Return data corresponding to the current conversation.
+        """
+        return JsonResponse({
+            'name': self.chatterbot.name
+        })
+
+# def test(request):
+#     res = {
+#         'reply': '呵呵'
+#     }
+#     return HttpResponse(json.dumps(res), content_type='application/json')
