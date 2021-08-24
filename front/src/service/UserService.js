c337c28752f62e3f6c164b7c2d71299e4f727c9d
wjn <2414666232@qq.com>
2021-08-24 21:10:16 +0800

    修改用户信息界面-补

--- front/src/service/UserService.js
+++ front/src/service/UserService.js
@@ -33,3 +33,43 @@ export const RegisterService = (
   const url = server + 'register/';
   postRequest(url, data, callback);
 };
+export const NameEditService = (username, newname, callback) => {
+  const data = {
+    username: username,
+    newname: newname,
+  };
+  const url = server + 'nameedit/';
+  postRequest(url, data, callback);
+};
+export const AvatarEditService = (username, imagedata, imagemime, callback) => {
+  const data = {
+    username: username,
+    imagedata: imagedata,
+    imagemime: imagemime,
+  };
+  const url = server + 'avataredit/';
+  postRequest(url, data, callback);
+};
+export const AvatarGetService = (username, callback) => {
+  const data = {
+    username: username,
+  };
+  const url = server + 'avatarget/';
+  postRequest(url, data, callback);
+};
+
+export const NicknameGetService = (username, callback) => {
+  const data = {
+    username: username,
+  };
+  const url = server + 'nicknameget/';
+  postRequest(url, data, callback);
+};
+
+export const EmailGetService = (username, callback) => {
+  const data = {
+    username: username,
+  };
+  const url = server + 'emailget/';
+  postRequest(url, data, callback);
+};
